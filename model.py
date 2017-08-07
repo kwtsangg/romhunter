bug fixed in calculation of Amp Factor Point Mass Lens

model.py
@@ -35,7 +35,7 @@ def AmpFactor_PointMassLens(freqList, M_Lz, y):
   vecList = []
   for ifreq in freqList:
     w = 8.*np.pi*M_Lz*ifreq*Msun_time
-    if abs(w) > 246.:
+    if abs(w) > 500.:
       vecList.append( AmpFactor_PointMassLens_geometrical(ifreq, M_Lz, y) )
     else:
       vecList.append( np.exp(np.pi*w/4. + 1j*w/2.*(np.log(w/2.)-2.*phim)) * complex(mpmath.gamma(1.-1j*w/2.) * mpmath.hyp1f1(1j*w/2., 1, 1j*w*y*y/2., maxterms=100000)) )
@@ -44,9 +44,10 @@ def AmpFactor_PointMassLens(freqList, M_Lz, y):
 
 def AmpFactor_PointMassLens_geometrical(f, M_Lz, y):
   y2 = y*y
+  Msun_time = 4.92567e-6
   abs_mu_p = np.abs(0.5 + (y2+2.)/(2.*y*np.sqrt(y2+4.)))
   abs_mu_m = np.abs(0.5 - (y2+2.)/(2.*y*np.sqrt(y2+4.)))
-  dt_d     = 4.*M_Lz*( y*np.sqrt(y2+4.) + np.log( (np.sqrt(y2+4.)+y)/(np.sqrt(y2+4.)-y) ) )
+  dt_d     = 4.*M_Lz*Msun_time*( y*np.sqrt(y2+4.)/2. + np.log( (np.sqrt(y2+4.)+y)/(np.sqrt(y2+4.)-y) ) )
   return np.sqrt(abs_mu_p) - 1j * np.sqrt(abs_mu_m) * np.exp(1j*2.*np.pi*f*dt_d)
 
 